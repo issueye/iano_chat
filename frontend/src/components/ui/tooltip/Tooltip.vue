a2c3d46cab75209440a8d154c75cf2612402cfcb
issueye <issueye@yeah.net>
2026-02-11 09:41:51 +0800

    style(ui): 为 Tooltip 组件添加 z-index 样式
    添加 z-[1000] 以确保 tooltip 内容始终显示在其他元素上方

--- frontend/src/components/ui/tooltip/Tooltip.vue
+++ frontend/src/components/ui/tooltip/Tooltip.vue
@@ -19,7 +19,7 @@ const props = defineProps({
       </TooltipTrigger>
       <TooltipPortal>
         <TooltipContent
-          class="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-grass11 select-none rounded-md bg-white px-[15px] py-[10px] text-sm leading-none shadow-sm border will-change-[transform,opacity]"
+          class="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-grass11 select-none rounded-md bg-white px-[15px] py-[10px] text-sm leading-none shadow-sm border will-change-[transform,opacity] z-[1000]"
           :side-offset="5"
         >
           {{ content }}
