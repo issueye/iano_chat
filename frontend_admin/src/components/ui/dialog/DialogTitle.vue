acfa37b69fb8a12e7e504510c5651d2706f981c6
issueye <issueye@yeah.net>
2026-02-13 15:51:03 +0800

    style(DialogTitle): 调整对话框标题关闭按钮的焦点样式

--- frontend_admin/src/components/ui/dialog/DialogTitle.vue
+++ frontend_admin/src/components/ui/dialog/DialogTitle.vue
@@ -18,7 +18,7 @@ const emit = defineEmits(['close'])
   >
     <slot />
     <button
-      class="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+      class="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:opacity-100 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
       @click="emit('close')"
       >
       <X class="h-4 w-4" />
